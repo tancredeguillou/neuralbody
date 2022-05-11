80b04b8c9b93c0185edd65bbf9635d3f3d3b4eef
Tancrede Guillou <tancredeguillou@gmail.com>
2022-05-11 11:27:29 +0200

    baseline code

--- lib/train/trainers/make_trainer.py
+++ lib/train/trainers/make_trainer.py
@@ -3,9 +3,7 @@ import imp
 
 
 def _wrapper_factory(cfg, network):
-    # module = lib.train.trainers.if_nerf_clight
     module = cfg.trainer_module
-    # path = lib/train/trainers/if_nerf_clight.py
     path = cfg.trainer_path
     network_wrapper = imp.load_source(module, path).NetworkWrapper(network)
     return network_wrapper
